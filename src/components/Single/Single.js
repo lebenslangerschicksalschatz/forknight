abada5d1d2d81c9e877790e14facf7db6e957461
kusokkulochok@gmail.com <kusokkulochok@gmail.com>
2020-04-30 14:54:13 +0300

    single slick gallery

--- src/components/Single/Single.js
+++ src/components/Single/Single.js
@@ -1,15 +1,22 @@
 import React, { useState, useEffect } from "react";
 import renderHTML from 'react-render-html';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { WORDPRESS_URL, POSTS_ENDPOINT } from "../const";
 import Loader from '../Loader';
 
 const Single = ({match}) => {
     useEffect (() => {
         fetchPost();
+        setSliders({mainSlider: "mainSlider", navSlider: "navSlider"})
     }, []);
 
     const [post, setPost] = useState({});
+    const [numberOfPhotos, setNumberOfPhotos] = useState(0);
     const [loading, setLoading] = useState(false);
+
+    const [sliders, setSliders] = useState({mainSlider: null, navSlider: null});
 
     async function fetchPost() {
         let url = WORDPRESS_URL+POSTS_ENDPOINT+match.params.id;
@@ -17,8 +24,26 @@ const Single = ({match}) => {
         const data = await fetchPost.json();
         setPost(data);
         setLoading(false);
-        console.log(data);
-    }    
+        setNumberOfPhotos(data.photos.length);
+    }
+
+    const settingsFor = {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        infinite: true,
+        asNavFor: sliders.navSlider
+    };
+
+    const settingsNav = {
+        slidesToShow: numberOfPhotos,
+        slidesToScroll: 1,
+        dots: false,
+        focusOnSelect: true,
+        infinite: true,
+        asNavFor: sliders.mainSlider
+    };
     
     return (        
         <div className="single">
@@ -38,17 +63,34 @@ const Single = ({match}) => {
                         {renderHTML(post.ingredients)}
                         </div>
                     <div className="single__content">{renderHTML(post.content.rendered)}</div>
-                    <div className="single-slider">
+                    <Slider 
+                    className="sl" 
+                    {...settingsFor}
+                    ref={slider => (sliders.mainSlider = slider)}>
                     {
                         post.photos.map((photo) => {
                             return (
-                                <div key={photo.id} className="single-slider__item">
+                                <div key={photo.id} className="sl__item">
                                     <img src={photo.guid} alt="recipe" />
                                 </div>
                             )
                         })
                     }
-                    </div>
+                    </Slider>
+                    <Slider 
+                    className="sl-nav" 
+                    {...settingsNav}
+                    ref={slider => (sliders.navSlider = slider)}>
+                    {
+                        post.photos.map((photo) => {
+                            return (
+                                <div key={photo.id} className="sl-nav__item">
+                                    <img src={photo.guid} alt="recipe" />
+                                </div>
+                            )
+                        })
+                    }
+                    </Slider>
                 </div> 
                 </>                
             }            
